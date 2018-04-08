8d7593cd4e46946ca3fc375c57813ffd3ed3f841
amjayy <33906054+amjayy@users.noreply.github.com>
2018-04-08 12:37:08 -0500

    Update for-loop.js

--- Javascript_4/javascript/for-loop.js
+++ Javascript_4/javascript/for-loop.js
@@ -1,4 +1,4 @@
-var scores = [24, 32, 17]; // Array of scores
+var scores = [24, 32, 17, 8,3, 45]; // Array of scores
 var arrayLength = scores.length // Items in array
 var roundNumber = 0; // Current round
 var msg = ''; // Message
